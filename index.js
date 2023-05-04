82784992d0473b82d1c5e6868b3ccdc7bfb1455f
mikabir76 <kabirmd0987@gmail.com>
2023-05-04 16:21:08 +0600

    chef Recipe json data added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const chefData = require('./data.json')
-
+const cors = require('cors')
 const app = express()
 const port = 5000;
 
-
+app.use(cors())
 app.get('/', (req, res)=>{
     res.send('Chef Recipe Server side Running')
 })
