9f29e2c49b09c164241e1ebfb05ae55e10e9e1d2
mikabir76 <kabirmd0987@gmail.com>
2023-05-05 21:04:01 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const chefData = require('./data.json')
 const cors = require('cors')
+const port = process.env.PORT || 5000;
 const app = express()
-const port = 5000;
 
 app.use(cors())
 app.get('/', (req, res)=>{
